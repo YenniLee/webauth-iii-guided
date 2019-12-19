61d1cc7441991d145cea835db373f42b27fb46be
Jennie Van <missjenvan@gmail.com>
2019-12-18 16:01:52 -0800

    notes from lecture

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const Users = require('../users/users-model.js');
 
@@ -25,7 +26,12 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        // sign token
+        const token = signToken(user);
+
+        // send the token
         res.status(200).json({
+          token,
           message: `Welcome ${user.username}!`,
         });
       } else {
@@ -36,5 +42,19 @@ router.post('/login', (req, res) => {
       res.status(500).json(error);
     });
 });
+
+function signToken(user) {
+  const payload = {
+    username: user.username
+  };
+
+  const secret = process.env.JWT_SECRET || 'is it secret?';
+
+  const options = {
+    expiresIn: '8h'
+  };
+
+  return jwt.sign(payload, secret, options)
+}
 
 module.exports = router;
--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,24 +1,47 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 
 const Users = require('../users/users-model.js');
 
 module.exports = (req, res, next) => {
-  const { username, password } = req.headers;
+  // const token = req.headers.authorization;
+  const { authorization } = req.headers;
+  if (authorization) {
+    const secret = process.env.JWT_SECRET || 'is it secret?';
 
-  if (username && password) {
-    Users.findBy({ username })
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          next();
-        } else {
-          res.status(401).json({ message: 'Invalid Credentials' });
-        }
-      })
-      .catch(error => {
-        res.status(500).json({ message: 'Ran into an unexpected error' });
-      });
+    jwt.verify(authorization, secret, function(err, decodedToken) {
+      if (err) {
+        res.status(401).json({ message: 'Invalid token.' })
+      } else {
+        req.token = decodedToken;
+  
+        next();
+      }
+    })
   } else {
-    res.status(400).json({ message: 'No credentials provided' });
+    res.status(400).json({ message: 'Unabe to login.' })
   }
+
 };
+
+// const { username, password } = req.headers;
+
+//   if (username && password) {
+//     Users.findBy({ username })
+//       .first()
+//       .then(user => {
+//         if (user && bcrypt.compareSync(password, user.password)) {
+//           next();
+//         } else {
+//           res.status(401).json({ message: 'Invalid Credentials' });
+//         }
+//       })
+//       .catch(error => {
+//         res.status(500).json({ message: 'Ran into an unexpected error' });
+//       });
+//   } else {
+//     res.status(400).json({ message: 'No credentials provided' });
+//   }
+// };
+
